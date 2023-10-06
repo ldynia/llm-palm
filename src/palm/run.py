59d50fab350be62e66dce96d4346bbdc3a69c07b
ldynia <l_dynowski@yahoo.com>
2023-10-06 20:02:29 +0200

    Create run.py

---
+++ src/palm/run.py
@@ -0,0 +1,11 @@
+import os
+import google.generativeai as palm
+
+
+PALM_KEY = os.getenv(PALM_KEY)
+
+palm.configure(api_key=PALM_KEY)
+
+response = palm.chat(messages='Hello')
+
+print(response.last)
